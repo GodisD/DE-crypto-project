9bc2d14e198882012da5685075f2e02f5e4dd334
Gui Dendena <guilherme.dendena@a3data.com.br>
2021-11-22 16:49:38 -0300

    teste

--- k8s-crypto/objects/binance_listener.py
+++ k8s-crypto/objects/binance_listener.py
@@ -11,26 +11,33 @@ class BinanceListener:
     def __init__(self, symbol) -> None:
         self.symbol = symbol
         self.client = Client(API_KEY, API_SECRET)
-        self.mongo_client = MongoClient(f'mongodb://{MONGO_USER}:{MONGO_PASSWORD}@127.0.0.1:27017/admin')
+        self.mongo_client = MongoClient(f'mongodb://localhost:27017')
         self.db = self.mongo_client[f'landing_sockets_{self.symbol}']
 
-    def handle_socket_message(self, msg, socket):
-        print(msg)
-        col = self.db[socket]
-        col.insert_one(msg)
+        for db in self.mongo_client.list_databases():
+            print(db)
+
+    def handle_socket_message(self, msg):
+        # print(msg)
+        col = self.db['book_depth']
+        res = col.insert_one(msg)
+        print(res)
+
+
+    def handle_futures_socket_message(self, msg):
+        col = self.db['bookTickerFutures']
+        res = col.insert_one(msg)
+        print("Arroz",res)
     
     def main(self):
 
-        status = self.client.get_system_status()
-        print(status)
-
-        info = self.client.get_exchange_info()
-        print(info)
 
         self.twm = ThreadedWebsocketManager(api_key=API_KEY, api_secret=API_SECRET)
         self.twm.start()
 
-        self.twm.start_depth_socket(symbol = self.symbol, callback=self.handle_socket_message(socket = 'depth_socket'))
+        self.twm.start_depth_socket(symbol = self.symbol, callback=self.handle_socket_message)
+        self.twm.start_futures_multiplex_socket(streams=[f"{self.symbol.lower()}@bookTicker"], callback=self.handle_futures_socket_message)
+
 
         self.twm.join()
 
