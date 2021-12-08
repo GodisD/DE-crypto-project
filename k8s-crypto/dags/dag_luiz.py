913664e66310744a6088d772f88631c24f41c45a
Gui Dendena <guilherme.dendena@a3data.com.br>
2021-12-08 15:03:15 -0300

    dag luiz

---
+++ k8s-crypto/dags/dag_luiz.py
@@ -0,0 +1,60 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator, PythonVirtualenvOperator
+from airflow.utils.dates import days_ago
+import requests
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+headers = {
+        'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36"
+        "(KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"
+        }
+
+
+def check_https(url):
+    response_code = {}
+    if isinstance(url, str):
+        if url.split('/')[0] in ['https:', 'http:']:
+            try:
+                response_code[url] = requests.get(url, timeout=30, headers=headers).text
+            except Exception:
+                response_code[url] = 'offline'
+        else:
+            try:
+                response_code[url] =  requests.get('https://' + url, timeout=30, headers=headers).text
+            except Exception:
+                try:
+                    response_code[url] = requests.get('http://' + url, timeout=30, headers=headers).text,
+                except Exception:
+                    response_code[url] = 'offline'
+    else:
+        pass
+    return response_code
+
+
+
+
+with DAG(
+    'dag_luiz',
+    default_args=default_args,
+    description='teste dag',
+    schedule_interval=days_ago(1),
+    start_date=datetime(2021, 12, 7),
+    catchup=False,
+    tags=['teste'],
+) as dag:
+    
+    virtualenv_task = PythonVirtualenvOperator(
+    task_id="check_https",
+    python_callable=check_https,
+    requirements=["requests"],
+    system_site_packages=True,
+)
+
